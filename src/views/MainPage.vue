ccd201e582756256be33c15194d1320f32e43026
PleBea <bini060315kim@gmail.com>
2022-07-01 21:02:37 +0900

    redesign: main page

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,10 +1,49 @@
 <template>
- <div></div>
+    <video class="background_video" src="@/assets/video/background.mp4" loop autoplay muted playsinline></video>
+    <div class="wrapper">
+        <header class="header">
+            <a href="/" class="logo">
+                <h1 class="title">0JIN &nbsp;2022</h1>
+            </a>
+        </header>
+        <section class="intro_section">
+            <div class="big_text_area">
+                <span class="text">
+                    IT TECHNOLOGY LEARNING CLUB<br />
+                    DEV & SECURITY & DESIGN
+                </span>
+            </div>
+        </section>
+        <section class="link_section">
+            <nav class="navigation">
+                <ul class="link_list">
+                    <li class="link_item">
+                        <a class="link" href="#">BLOG</a>
+                    </li>
+                    <li class="link_item">
+                        <a class="link" href="#">DATA</a>
+                    </li>
+                    <li class="link_item">
+                        <a class="link" href="#">CONTACT</a>
+                    </li>
+                </ul>
+            </nav>
+        </section>
+        <footer class="footer">
+            <div class="description_wrapper">
+                <p class="description">L : SEOUL, REPUBLIC OF KOREA</p>
+                <div class="description_under">
+                    <p class="description">T : 010.3126.4485</p>
+                    <p class="description">M : 0jin@khjco.de</p>
+                </div>
+            </div>
+        </footer>
+    </div>
 </template>
 
 <script>
-
 export default {
+
 }
 </script>
 
